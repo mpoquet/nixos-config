bdf4735a7990e8eda3359bc1adc48ca5d7108426
Millian Poquet <millian.poquet@inria.fr>
2019-09-03 18:05:37 +0200

    refactor

--- laptop-datamove.nix
+++ laptop-datamove.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hw-configuration-laptop-thesis.nix
+      ./hardware/laptop-datamove.nix
     ];
 
   boot.cleanTmpDir = true;
